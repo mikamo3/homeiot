becd2136185f0b38dd3bb1059d104e30b2208eac
mikamo3 <kamo3proj@gmail.com>
2023-03-15 02:45:29 +0900

    :bug: (log) set priority

--- lib/logger.go
+++ lib/logger.go
@@ -1,13 +1,34 @@
 package lib
 
-import "go.uber.org/zap"
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
 
 var Logger *zap.Logger
 
 func init() {
-	var err error
-	Logger, err = zap.NewProduction()
-	if err != nil {
-		panic(err)
-	}
+	Logger = newMultiOutputLogger()
+}
+func newMultiOutputLogger() *zap.Logger {
+	highPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.ErrorLevel
+	})
+	lowPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl < zapcore.ErrorLevel
+	})
+	stdoutSink := zapcore.Lock(os.Stdout)
+	stderrSink := zapcore.Lock(os.Stderr)
+
+	enc := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+
+	core := zapcore.NewTee(
+		zapcore.NewCore(enc, stderrSink, highPriority),
+		zapcore.NewCore(enc, stdoutSink, lowPriority),
+	)
+
+	logger := zap.New(core)
+	return logger
 }
